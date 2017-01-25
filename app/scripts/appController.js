da24a19f258ad11fc299d1303a1d156da8bf4dd1
Diego Santos Seabra <dss-diego@hotmail.com>
2017-01-24 23:57:05 -0200

    Módulo de Criação de Arquivo (File API Jetpack)
    js, json, rnl, etc

--- app/scripts/appController.js
+++ app/scripts/appController.js
@@ -9,9 +9,16 @@
     var platform = 'android';
     var layout = 'code';
 
+    // Jetpack - File API Manager
+    var jetpack = require('fs-jetpack');
+    var destCode = jetpack.dir('export/code');
+    var destImages = jetpack.dir('export/images');
+    var temp = jetpack.dir('temp', { empty: true });
+
 	angular
 		.module('app', ['ngMaterial', 'ngAnimate'])
-		.controller('AppController', ['$scope', 'logger', AppController]);
+		.controller('AppController', ['$scope', 'logger', AppController])
+    .controller('ComponentController', ['$scope', 'logger', ComponentController])
 
 	function AppController($scope, logger) {
 
@@ -139,40 +146,16 @@
           window.close();
       }
 
+      // Component Creation
+      this.createText = function(content) {
+        console.log('Entrou createText');
+        temp.write('Sem Titulo.rnl', content);
+      }
+
 	}
 
-	angular
-    .module('menuDemoBasic', ['ngMaterial'])
-    .config(function($mdIconProvider) {
-      $mdIconProvider
-        .iconSet("call", 'img/icons/sets/communication-icons.svg', 24)
-        .iconSet("social", 'img/icons/sets/social-icons.svg', 24);
-    })
-    .controller('BasicDemoCtrl', function DemoCtrl($mdDialog) {
-      var originatorEv;
+  function ComponentController($scope, logger) {
 
-      this.notificationsEnabled = true;
-      this.toggleNotifications = function() {
-        this.notificationsEnabled = !this.notificationsEnabled;
-      };
-
-      this.redial = function() {
-        $mdDialog.show(
-          $mdDialog.alert()
-            .targetEvent(originatorEv)
-            .clickOutsideToClose(true)
-            .parent('body')
-            .title('Suddenly, a redial')
-            .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
-            .ok('That was easy')
-        );
-
-        originatorEv = null;
-      };
-
-      this.checkVoicemail = function() {
-        // This never happens.
-      };
-    });
-
+  }
+	
 })();
