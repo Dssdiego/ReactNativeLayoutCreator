ae705479e66cbe75722b99dd4414e10f714a5d35
Diego Santos Seabra <dss-diego@hotmail.com>
2017-01-26 20:41:07 -0200

    Inserção de Color Picker para Fundo do Layout

--- app/scripts/appController.js
+++ app/scripts/appController.js
@@ -19,10 +19,28 @@
     var contNewScene = 0;
 
 	angular
-		.module('app', ['ngMaterial', 'ngAnimate'])
+		.module('app', ['ngMaterial', 'ngAnimate', 'tb-color-picker'])
 		.controller('AppController', ['$scope', 'logger', AppController]);
 
 	function AppController($scope, logger) {
+
+    // Color Picker
+    $scope.colorPickerOptions = [
+          'transparent','#e51c23', 
+          '#E91E63', '#9C27B0', 
+          '#673AB7', '#3F51B5', 
+          '#2196F3', '#03A9F4',
+          '#00BCD4', '#009688',
+          '#4CAF50', '#8BC34A',
+          '#CDDC39', '#FFEB3B',
+          '#FFC107', '#FF9800',
+          '#FF5722', '#795548',
+          '#9E9E9E', '#607D8B'];
+    $scope.colorPickerMainColor = '#e51c23';
+
+    $scope.colorPickerChanged = function(newColor, oldColor) {
+        console.log('from ', oldColor, ' to ', newColor);
+    }
 
 		// Tabs do Canvas de Desenho
 		var tabs = [],
